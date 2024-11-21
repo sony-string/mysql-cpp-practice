fd51da7b544de29fde9e41202b23e1c17ba0ace4
SONY-STRING <sctoa1@gmail.com>
2024-11-21 23:01:30 +0900

    feat(Book): DB의 Book 테이블에 대한 간단한 읽기, 삽입, 삭제 API 구현

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,57 @@
 #include <mysql/mysql.h>
+#include <mysql_error.h>
 #include <iostream>
 #include <cstdlib>
 
+void search_books(MYSQL *conn) {
+    if (mysql_query(conn, "SELECT * FROM Book")) {
+        std::cerr << "Query error: " << mysql_error(conn) << std::endl;
+        return;
+    }
+
+    MYSQL_RES *res = mysql_store_result(conn);
+
+    if (res == nullptr) {
+        std::cerr << "Store result error: " << mysql_error(conn) << std::endl;
+        return;
+    }
+
+    int num_fields = mysql_num_fields(res);
+    MYSQL_ROW row;
+    while ((row = mysql_fetch_row(res))) {
+        for(int i = 0; i < num_fields; i++) {
+            std::cout << (row[i] ? row[i] : "NULL") << "\t";
+        }
+        std::cout << std::endl;
+    }
+
+    mysql_free_result(res);
+}
+
+void insert_book(MYSQL *conn, int bookid, const std::string &bookname, const std::string &publisher, int price) {
+    std::string query = "INSERT INTO Book (bookid, bookname, publisher, price) VALUES (" +
+        std::to_string(bookid) + ", '" + bookname + "', '" + publisher + "', " + std::to_string(price) + ")";
+    
+    if (mysql_query(conn, query.c_str())) {
+        return;
+    }
+
+    std::cout << "Book inserted successfully" << std::endl;
+}
+
+void deleteBook(MYSQL *conn, int bookid) {
+    std::string query = "DELETE FROM Book WHERE bookid = " + std::to_string(bookid);
+    
+    if (mysql_query(conn, query.c_str())) {
+        return;
+    }
+
+    std::cout << "Book deleted successfully" << std::endl;
+}
+
+
 int main() {
     MYSQL *conn;
-    MYSQL_RES *res;
-    MYSQL_ROW row;
 
     const char *server      = std::getenv("MYSQL_SERVER");  
     const char *user        = std::getenv("MYSQL_USER");  
@@ -20,30 +66,30 @@ int main() {
         return 1;
     }
 
-    if (mysql_query(conn, "SELECT * FROM Book")) {
-        std::cerr << "Query error: " << mysql_error(conn) << std::endl;
-        mysql_close(conn);
-        return 1;
-    }
+    while (true) {
+        int query_num;
+        std::cout << "1. search\t2. insert\t3. delete" << std::endl;
+        std::cin >> query_num;
 
-    res = mysql_store_result(conn);
+        if (query_num == 1) {
+            search_books(conn);
+        } else if (query_num == 2) {
+            int bookid, price;
+            std::string bookname, publisher;
 
-    if (res == nullptr) {
-        std::cerr << "Store result error: " << mysql_error(conn) << std::endl;
-        mysql_close(conn);
-        return 1;
-    }
+            std::cout << "<bookid> <bookname> <publisher> <price>" << std::endl;
+            std::cin >> bookid >> bookname >> publisher >> price;
+            insert_book(conn, bookid, bookname, publisher, price);
+        } else if (query_num == 3) {
+             int bookid;
 
-    int num_fields = mysql_num_fields(res);
-    while ((row = mysql_fetch_row(res))) {
-        for(int i = 0; i < num_fields; i++) {
-            std::cout << (row[i] ? row[i] : "NULL") << "\t";
+             std::cout << "<bookid>" << std::endl;
+             std::cin >> bookid;
+             deleteBook(conn, bookid);
         }
-        std::cout << std::endl;
     }
-
-    // 결과 해제
-    mysql_free_result(res);
+    
+    
     mysql_close(conn);
 
     return 0;
